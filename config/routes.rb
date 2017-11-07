51932972b67f28203e87018fcf5e356da2ec8279
EdwinnaElbert <edwinnaelbert@ukr.net>
2017-11-07 20:34:39 +0300

    access rights

--- config/routes.rb
+++ config/routes.rb
@@ -1,10 +1,15 @@
 Rails.application.routes.draw do
-
-  resources :posts
+  # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+  resources :posts, only: [:index, :show]
+  root to: "posts#index"
+#   namespace :user do
+#     resources :posts, only: [:index, :show, :edit, :update, :destroy]
+#   end
+  authenticated :user do
+    resources :posts #, as: :authenticated_root
+  end
   if Rails.env.development?
     mount LetterOpenerWeb::Engine, at: "/letter_opener"
   end
   devise_for :users
-  # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
-  root to: "posts#index"
 end
